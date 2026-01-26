4a2dc3f454e58dd3163aa729118c167c26629746
maaitlunghau <chunhau.py@gmail.com>
2026-01-26 23:52:50 +0700

    ui(landing-page): remove services component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -22,7 +22,6 @@ export default async function Home() {
       <Hero />
       <Benefits />
       <Faq />
-      <Services />
       <Feedback />
       <ServicesPricingCardModern />
       <Clients />
